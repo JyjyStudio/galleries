32f60c7befff4bdf219a55bb4ffd69a25c2d3d37
Jyjystudio <jyhad.benyoussef@gmail.com>
2022-02-02 21:36:18 +0100

    fix code

--- src/ts/randomImage.ts
+++ src/ts/randomImage.ts
@@ -1,37 +1,32 @@
-//Get #pageContent element
-let pageContent = <HTMLElement>document.getElementById('pageContent');
-//get #pictInfo div
+let images=[{name:'image_1.jpeg',title:'Modèle : lamborghini Countach',annee:'Année : 1999',Motorisation:'Motorisation : 5.5L'},{name:'image_2.jpeg',title:'Modèle : Mercedes Class C AMG',annee:'Année : 2012',Motorisation:'Motorisation : 4.5L '},{name:'image_3.jpeg',title:'Modèle : BMW M4',annee:'Année : 2019',Motorisation:'Motorisation : 3.0L '},{name:'image_4.jpeg',title:'Modèle : Pagani Zonda',annee:'Année : 2020',Motorisation:'Motorisation : 7.0L'},{name:'image_5.jpeg',title:'Modèle : Chevrolet',annee:'Année : 1952',Motorisation:'Motorisation : 2.0L'},{name:'image_6.jpeg',title:'Modèle : Ferrari',annee:'Année : 1986',Motorisation:'Motorisation : 5.2L'},{name:'image_7.jpeg',title:'Modèle : Bugatti Divo',annee:'Année : 2021',Motorisation:'Motorisation : 8.0L'},{name:'image_8.jpeg',title:'Modèle : Mercedes G63 AMG',annee:'Année : 2018',Motorisation:'Motorisation : 6.3L'},{name:'image_9.jpeg',title:'Modèle :Ford Mustang',annee:'Année : 1969',Motorisation:'Motorisation : 4.0L'},{name:'image_10.jpeg',title:'Modèle : Audi R8',annee:'Année : 2013',Motorisation:'Motorisation : 5.5L'},{name:'image_11.jpeg',title:'Modèle : Land Rover Defender',annee:'Année : 1995',Motorisation:'Motorisation : 2.0L'},{name:'image_12.jpeg',title:'Modèle : Ford GT',annee:'Année : 1953',Motorisation:'Motorisation : 4.0L'},{name:'image_13.jpeg',title:'Modèle : Volkwagen Combi',annee:'Année : 1966',Motorisation:'Motorisation : 1.6L'},{name:'image_14.png',title:'Modèle : Lamborghini Huracan',annee:'Année : 2016',Motorisation:'Motorisation : 4.5L'}]
+
+//fond d'ecran
+let background = <HTMLElement>document.getElementById('pageContent');
+//url aleatoire
+let random_image = images[Math.floor(Math.random() * images.length)];
+let random_url = `url(images/${random_image.name})`;
+background.style.backgroundImage = random_url;
+//section avec titre et paragraphes
 let pictInfo = document.getElementById('pictInfo');
-//Define the images array
-let rawImages = '[{"filename":"image-1.jpg","name":"Plage de Bretagne","author":"Damien Bruyndonckx","exposure":"100 ISO - 18 mm - f/22 - 302 Sec"},{"filename":"image-2.jpg","name":"Cadre dans le cadre","author":"Damien Bruyndonckx","exposure":"200 ISO - 150 mm - f/10 - 1/320 Sec"},{"filename":"image-3.jpg","name":"Vigie minérale","author":"Damien Bruyndonckx","exposure":"100 ISO - 59 mm - f/13 - 1/250 Sec"},{"filename":"image-4.jpg","name":"Les cheveux du ciel","author":"Damien Bruyndonckx","exposure":"100 ISO - 18 mm - f/22 - 531 Sec"},{"filename":"image-5.jpg","name":"Prendre le large","author":"Damien Bruyndonckx","exposure":"100 ISO - 128 mm - f/11 - 1/250 Sec"},{"filename":"image-6.jpg","name":"Maman les p\'tits bateaux","author":"Damien Bruyndonckx","exposure":"100 ISO - 150 mm - f/28 - 1/320 Sec"},{"filename":"image-7.jpg","name":"L\'ombre des palmiers","author":"Damien Bruyndonckx","exposure":"100 ISO - 26 mm - f/10 - 1/100 Sec"},{"filename":"image-8.jpg","author":"Damien Bruyndonckx","name":"Pont d\'or","exposure":"100 ISO - 37 mm - f/11 - 1/400 Sec"},{"filename":"image-9.jpg","name":"L\'ombre assise","author":"Damien Bruyndonckx","exposure":"100 ISO - 24 mm - f/5 - 1/200 Sec"},{"filename":"image-10.jpg","name":"Au bout de l\'ombre","author":"Damien Bruyndonckx","exposure":"200 ISO - 150 mm - f/6.3 - 1/320 Sec"},{"filename":"image-11.jpg","name":"CA Capitol","author":"Damien Bruyndonckx","exposure":"200 ISO - 39 mm - f/10 - 1/640 Sec"},{"filename":"image-12.jpg","name":"Soleil tropical","author":"Damien Bruyndonckx","exposure":"100 ISO - 20 mm - f/11 - 1/80 Sec"},{"filename":"image-13.jpg","name":"Reflet urbain","author":"Damien Bruyndonckx","exposure":"200 ISO - 39 mm - f/13 - 1/200 Sec"},{"filename":"image-14.jpg","name":"Lucide?","author":"Damien Bruyndonckx","exposure":"200 ISO - 70 mm - f/8.0 - 1/100 Sec"}]';
-let images = JSON.parse(rawImages);
-
-//Choose a random image
-let randomImage = images[Math.floor(Math.random() * images.length)];
-
-//apply the image as the background of pageContent
-pageContent.style.backgroundImage = `url(images/${randomImage.filename})`;
-
-if(pictInfo){
-    //Empty the pictInfo div
+//Page accueil : efface les infos de l'image precedente avant le chargement de la page
+if (pictInfo) {
     while(pictInfo.firstChild){
-        pictInfo.removeChild(pictInfo.firstChild);
+        pictInfo.removeChild(pictInfo.firstChild)
     }
-
-    //Create pictInfo title and add it to the Div
-    let pictTitle = document.createElement('h1');
-    let pictTitleContent = document.createTextNode(randomImage.name);
-    pictTitle.appendChild(pictTitleContent);
-    pictInfo.appendChild(pictTitle);
-
-    //Create author and exposure P and add them to pictInfo
-    let pictauthor = document.createElement('p');
-    let pictData = document.createElement('p');
-    let pictAuthorContent = document.createTextNode(randomImage.author);
-    let pictDataContent = document.createTextNode(randomImage.exposure);
-    pictauthor.appendChild(pictAuthorContent);
-    pictData.appendChild(pictDataContent);
-    pictInfo.appendChild(pictauthor);
-    pictInfo.appendChild(pictData);
-
+    //ajoute les infos de la nouvelle image lors du chargement d'une nouvelle page
+    //ajout du titre 
+    let titre = document.createElement('h1');
+    let pictTile = document.createTextNode(random_image.title);
+    titre.appendChild(pictTile);
+    pictInfo.appendChild(titre)
+    //ajout de l'annee
+    let premierParagraphe = document.createElement('p');
+    let annee = document.createTextNode(random_image.annee);
+    premierParagraphe.appendChild(annee);
+    pictInfo.appendChild(premierParagraphe);
+    //ajout de la cylindree
+    let secondParagraphe = document.createElement('p');
+    let cylindree = document.createTextNode(random_image.Motorisation);
+    secondParagraphe.appendChild(cylindree);
+    pictInfo.appendChild(secondParagraphe);
 }
